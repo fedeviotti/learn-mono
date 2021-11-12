a1b0a181a22c5fa9ed94b85904a341052c759d60
fedeviotti <fedeviotti@gmail.com>
2021-11-12 14:50:52 +0100

    Initial commit

---
+++ packages/app/src/App.js
@@ -0,0 +1,14 @@
+import Text from '@fedeviotti/common/src/components/text';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Text />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/common/src/components/text.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Text = () => {
+    return <h1>Hi from me!!!</h1>
+}
+
+export default Text;
